d76f61b4477f89b01782c034b1b1a0407fdbb7ec
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-12-01 20:44:25 +0000

    delete multiple/all records

--- sql-crud.py
+++ sql-crud.py
@@ -115,26 +115,31 @@ session.add(barbara_liskov)
 #         person.gender = "Male"
 #     else:
 #         print("Gender not defined")
-#     session.commit()    
+#     session.commit()
 
 
 # deleting a single record
-fname = input("Enter a first name: ")
-lname = input("Enter a last name: ")
-programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
 # defensive programming
-if programmer is not None:
-    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
-    confirmation = input("Are you sure you want to delete this record? (y/n): ")
-    if confirmation.lower() == "y":
-        session.delete(programmer)
-        session.commit()
-        print("Programmer Deleted")
-    else:
-        print("Programmer not deleted")    
-else:
-    print("No records found") 
-           
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n): ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer Deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
+# delete multiple/all records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
 
 
 # query the database to find all Programmers
